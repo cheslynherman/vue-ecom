9449528737abe9d44a1fd6fd239c4a9d2668e55d
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-06-27 16:36:10 +0200

    /

---
+++ src/views/ProductView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div v-if="product" class="product-details">
+        <img :src="product.image_url" :alt="product.name">
+        <h2>{{product.name}}</h2>
+        <p>R {{product.price}} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["id"],
+    computed: {
+        product() {
+            return this.$store.state.product;
+        },
+    },
+    mounted (){
+        this.$store.dispatch("getProduct",this.id);
+    },
+};
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-if="products" class="flex-container">
+        <CardComp v-for="product of products" :key="product.id" :product="tutorial"/>
+    </div>
+    <div v-else>Loading...</div>
+</template>
+
+<script>
+import CardComp from "@/components/Card-comp.vue"
+export default {
+    computed: {
+        products () {
+            return this.$store.state.products
+        },
+        mounted() {
+            this.$store.state.dispatch("getProducts")
+        },
+    },
+
+    components: {CardComp}
+}
+</script>
